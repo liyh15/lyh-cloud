e48a10432b8d537f96e7615a7e93beaf36cfc9fe
lyh <404565548@qq.com>
2020-07-24 11:46:05 +0800

    添加

--- lyh-cloud/lyh-cloud-security-server/src/main/java/wibo/cloud/custom/mq/RabbitMQConsumer.java
+++ lyh-cloud/lyh-cloud-security-server/src/main/java/wibo/cloud/custom/mq/RabbitMQConsumer.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 public class RabbitMQConsumer {
@@ -15,7 +17,7 @@ public class RabbitMQConsumer {
     public static String a = "BBB";
 
     public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, TimeoutException {
-        RabbitMQConsumer.consumer();
+        RabbitMQConsumer.queueParam();
     }
 
     /**
@@ -118,6 +120,45 @@ public class RabbitMQConsumer {
         // 关闭自动消息确认，autoAck = false
         channel.basicConsume("delay", false, consumer);
     }
+
+    /**
+     * 消费消息
+     */
+    public static void queueParam() throws URISyntaxException, IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException {
+        Connection connection = getConnection();
+        Channel channel = connection.createChannel();
+        Map<String, Object> args = new HashMap<String, Object>();
+        // args.put("x-message-ttl", 5000); // 设置队列中消息的过期时间（未被消费者消费的消息，已被消费的消息还未被确认的不会被删除）
+        // args.put("x-expires", 5000); // 表示没有与消费者绑定的队列最大的删除时间
+        args.put("x-max-length", 2); // 设置队列最多存放的消息条数，如果超过数量，队列头部的消息将被删除
+        // args.put("x-max-length-bytes", 10); // 队列可以容纳的最大字节数，超过这个字节数，队列头部的消息将会被删除
+        // args.put("x-overflow", "reject-publish"); // 设置消息拒绝策略，“拒收消息”
+        // args.put("x-dead-letter-exchange", "deadExchange"); // 该参数值为一个(死信)交换机的名称,当队列中的消息的生存期到了,或者因长度限制被丢弃时,消息会被推送到(绑定到)这台交换机(的队列中),而不是直接丢掉
+        args.put("x-dead-letter-routing-key", "test");
+        channel.queueDeclare("Test_Queues", false, false, false, args);
+        /*channel.basicQos(1);
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body);
+                System.out.println("已经处理完" + message);
+                // 消息确认
+                try {
+                    Thread.sleep(10000);
+                    *//**
+                     * 第一个参数表示投递标签，从1开始每次增加1，用于唯一标识信道的每次投递，
+                     * 第二个参数表示是否进行消息的批量确认。若确认消息时开启批量确认，则投递标签小于当前消息投递标签的所有消息也都会进行确认。
+                     *//*
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                } catch (IOException | InterruptedException e) {s
+                    e.printStackTrace();
+                }
+            }
+        };
+        // 关闭自动消息确认，autoAck = false
+        channel.basicConsume("Test_Queues", false, consumer);*/
+    }
+
 
 
     public static Connection getConnection() throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, TimeoutException {
